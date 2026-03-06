9126b01b9546db2f7b2301eb5c00019d17d7707f
garoseronerozero <265354211+garoseronerozero@users.noreply.github.com>
2026-03-07 00:18:10 +0900

    Update username_aliases.js

--- data/username_aliases.js
+++ data/username_aliases.js
@@ -1,11 +1,7 @@
 const username_aliases = {
-
-"Happipupper": { label: "귀여운기린" },
-"iamshipduck1": { label: "오리선장" },
-"0825_0717": { label: "웃는여우" },
-"sn7__________": { label: "나른한거북이" },
-"feministsuhyun": { label: "포근아기물범" }
-
+  "Happipupper": { label: "귀여운기린" },
+  "iamshipduck1": { label: "오리선장" },
+  "0825_0717": { label: "웃는여우" },
+  "sn7__________": { label: "나른한거북이" },
+  "feministsuhyun": { label: "포근아기물범" }
 }
-
-export default username_aliases
