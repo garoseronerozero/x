cbcb2f54eb01950c0e2a5deb364bbfa7817e34cf
zero <garoseronerozero@users.noreply.github.com>
2026-03-07 23:48:38 +0900

    .

--- data/username_aliases.js
+++ data/username_aliases.js
@@ -3,8 +3,10 @@ const username_aliases = {
   "iamshipduck1": { label: "오리선장" },
   "0825_0717": { label: "웃는여우" },
   "sn7__________": { label: "나른한거북이" },
+  // "mamyu1490796": { label: "웃는돌고래" },
+  // "gyb0lICcD310210": { label: "신나는다람쥐" },
+  // "feministsuhyun": { label: "포근아기물범" },
   "aina0660": { label: "햇살강아지" },
-  "mamyu1490796": { label: "웃는돌고래" },
-  "gyb0lICcD310210": { label: "신나는다람쥐" },
-  "feministsuhyun": { label: "포근아기물범" }
+  "hwahwa85221": { label: "어리둥절쿼카" },
+  "sariseyo_jebal": { label: "꾸벅꾸벅판다" },
 }
