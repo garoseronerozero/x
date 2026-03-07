e44fd5f57eb80cf2648e39b5e2161cf8bbfeedbe
zero <garoseronerozero@users.noreply.github.com>
2026-03-08 07:19:09 +0900

    .

--- data/tweets.js
+++ data/tweets.js
@@ -31,7 +31,6 @@ const tweets = [
 "https://x.com/0825_0717/status/2029441007067250770",
 "https://x.com/aina0660/status/2027774495101227120",
 "https://x.com/aina0660/status/2027773802260926798",
-"https://x.com/aina0660/status/2027773802260926798",
 "https://x.com/hwahwa85221/status/2028415481506582603",
 "https://x.com/sariseyo_jebal/status/1905271625735377306",
 "https://x.com/sariseyo_jebal/status/1947793179803324790",
