f7e5ae896105fec6b9b11cb46f5034edd4064193
zero <garoseronerozero@users.noreply.github.com>
2026-03-08 14:38:20 +0900

    .

--- data/username_aliases.js
+++ data/username_aliases.js
@@ -28,7 +28,7 @@ const username_aliases = {
   "choxchco2103": { label: "달보는수달" },
   "_cospirit": { label: "포동햄스터" },
   "pleasedont1112": { label: "귤까는너구리" },
-  // "yulhwa9909": { label: "풀뜯는토끼" },
+  "shitmoneythumb": { label: "풀뜯는토끼" },
   // "yulhwa9909": { label: "낮잠자는판다" },
   // "yulhwa9909": { label: "조용한아기여우" },
 
